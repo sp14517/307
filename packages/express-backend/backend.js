cb5e8a8463ef74c1fa7531764819a817d646283c
Sharon Pagidipati <sp14517@gmail.com>
2024-04-15 13:41:15 -0700

    backend part 5 works

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -63,3 +63,16 @@ const users = {
       res.send(users);
     }
   });
+
+  const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
