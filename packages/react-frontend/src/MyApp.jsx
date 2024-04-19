6df16616926e22544872dce03169a96771657a30
Sharon Pagidipati <sp14517@gmail.com>
2024-04-19 12:24:04 -0700

    part 3 of lab 3 done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -17,14 +17,28 @@ import Form from "./Form";
         });
         setCharacters(updated);
       }
-      function updateList(person) {
-        setCharacters([...characters, person]);
-    
-      }
+      function updateList(person) { 
+        postUser(person)
+          .then(() => setCharacters([...characters, person]))
+          .catch((error) => {
+            console.log(error);
+          })
+    }
       function fetchUsers() {
         const promise = fetch("http://localhost:8000/users");
         return promise;
     }
+    function postUser(person) {
+        const promise = fetch("Http://localhost:8000/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(person),
+        });
+    
+        return promise;
+      }
     useEffect(() => {
         fetchUsers()
             .then((res) => res.json())
